56b8bf18bc63f3525e3f22ab22f7ee5c51599896
Stanley Salvatierra <stanlee321@gmail.com>
2020-03-22 08:09:24 -0400

    updated api for handle query paramteres for spreadszheet id , sheet name and colend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,26 +4,28 @@ import { google } from 'googleapis'
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-const spreadsheet = functions.config().googleapis.sheets
+// const spreadsheet = functions.config().googleapis.sheets
 
 
 export const helloWorld = functions.https.onRequest((request, response) => {
 
-    
+    const spreadSheetId  = request.query.sheedId
+    const sheet = request.query.sheet
+    const colEnd = request.query.colEnd
+
     google.auth.getClient({
         // see other scopes here: https://developers.google.com/sheets/api/guides/authorizing
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     }).then( async  auth => {
         const api = google.sheets({ version: 'v4', auth })
+
         const getSheets = api.spreadsheets
-        
-        console.log("... spreed", spreadsheet)
-        
+                
         const sheetVals = await getSheets
             .values
             .get({ 
-                spreadsheetId: spreadsheet.id,
-                range: `Data!A2:K` 
+                spreadsheetId: spreadSheetId,
+                range: `${sheet}!A2:${colEnd}` 
             });    
 
         if (sheetVals.status < 200 || sheetVals.status > 299) {
