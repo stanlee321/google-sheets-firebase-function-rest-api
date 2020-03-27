bea0137fc3a8c628ce9e0e19f10c705d9bed6f96
Stanley Salvatierra <stanlee321@gmail.com>
2020-03-27 09:03:34 -0400

    uptated API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,38 +11,6 @@ import { appendEntry } from './append';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
 
-<<<<<<< HEAD
-    console.log(request.query)
-
-    const spreadSheetId  = request.query.sheetId
-    const sheet = request.query.sheet
-    const start = request.query.start
-    const colEnd = request.query.colEnd
-
-    google.auth.getClient({
-        // see other scopes here: https://developers.google.com/sheets/api/guides/authorizing
-        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    }).then( async  auth => {
-        const api = google.sheets({ version: 'v4', auth })
-
-        const getSheets = api.spreadsheets
-                
-        const sheetVals = await getSheets
-            .values
-            .get({ 
-                spreadsheetId: spreadSheetId,
-                range: `${sheet}!${start}:${colEnd}` 
-            });    
-
-        if (sheetVals.status < 200 || sheetVals.status > 299) {
-            console.log('error reading sheet')
-            return
-        }
-        response.send(sheetVals);
-        
-        }).catch( (e) => {
-            console.log(e)
-=======
 
     if (request.method == "POST") {
         console.log("POST")
@@ -72,7 +40,6 @@ export const helloWorld = functions.https.onRequest((request, response) => {
             .catch(err => {
                 console.warn('error writing to spreadsheet', err)
             })           
->>>>>>> bfca0da832a5dadf0fedefea335582e177856747
         })
         .catch((e) => console.log(e))
 
